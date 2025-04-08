6bbb6122d18537dd0c47b214f812851fca10e985
Wiktor Sędzimir <wiktor.sedzimir@gmail.com>
2025-04-08 14:10:15 +0200

    Add simulation running and writing output to file logic in Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,17 @@
+import model.enums.Direction;
+import model.enums.Move;
 import model.input.InputReader;
+import model.output.OutputCollector;
+import model.simulation.Simulation;
+import model.simulation.SimulationRunner;
+import model.traffic.Road;
+import model.traffic.TrafficLane;
 
+import java.io.FileWriter;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,12 +31,53 @@ public class Main {
         Map<String, List<Map<String, String>>> commands = optionalInput.get();
         List<Map<String, String>> commandsList = commands.get("commands");
 
-        System.out.println(commands.get("commands"));
+        if (commandsList == null) {
+            System.out.println("Invalid input!!!");
+            return;
+        }
 
-        for (Map<String, String> command : commandsList) {
-            System.out.println(command.get("type"));
+        OutputCollector outputCollector = new OutputCollector();
+        Simulation singleLanesSimulation = getSingleLanesSimulation();
+        singleLanesSimulation.subscribe(outputCollector);
+
+        SimulationRunner simulationRunner = new SimulationRunner(singleLanesSimulation);
+        simulationRunner.runSimulation(commandsList);
+
+        String pathToOutputFile = args[1];
+
+        try (FileWriter outputFile = new FileWriter(pathToOutputFile)) {
+            outputFile.write(outputCollector.toString());
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
         }
     }
 
+    private static Simulation getSingleLanesSimulation() {
+        Set<Move> all = Set.of(Move.RIGHT, Move.STRAIGHT, Move.LEFT);
 
+        Road roadNorth = new Road(
+                Direction.NORTH,
+                List.of(
+                        new TrafficLane(all)
+                ));
+        Road roadEast = new Road(
+                Direction.EAST,
+                List.of(
+                        new TrafficLane(all)
+                ));
+        Road roadSouth = new Road(
+                Direction.SOUTH,
+                List.of(
+                        new TrafficLane(all)
+                ));
+        Road roadWest = new Road(
+                Direction.WEST,
+                List.of(
+                        new TrafficLane(all)
+                ));
+
+        return new Simulation(List.of(
+                roadNorth, roadEast, roadSouth, roadWest
+        ));
+    }
 }
